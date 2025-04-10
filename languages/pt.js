dd28a9c4b7c32de09983dd6116ac597b81795a77
tanvirahmed22179 <tanvirahmedshakil60@gmail.com>
2025-04-11 02:59:01 +0400

    Update pt.js

--- languages/pt.js
+++ languages/pt.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Desenvolvido por SSRR | Prime Music v1.2",
+    footer: "Desenvolvido por TANVIR | ScOut Music v1.2",
     ping: {
       description: "Verificar latência do bot",
       response: "Medindo latência...",
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Tempo de resposta do bot: **{latency}ms**",
         websocketPing: "- Ping do WebSocket: **{ping}ms**",
         uptime: "- Tempo online: **{uptime}**",
-        footer: "Desenvolvido por SSRR | Prime Music v1.2"
+        footer: "Desenvolvido por TANVIR | ScOut Music v1.2"
       }
     },
     addsong: {
